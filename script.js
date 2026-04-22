ac7ca0a7c7b97eebc0dcfd7eb907987c1d3ba36a
o-lehner <olivierlehner1@gmail.com>
2026-04-22 22:30:30 +0200

    Zmniejszono liczbe kwiatkow o 40% na mobilkach

--- script.js
+++ script.js
@@ -11,7 +11,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const pauseIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><rect x="6" y="4" width="4" height="16"></rect><rect x="14" y="4" width="4" height="16"></rect></svg>';
 
     // --- Flower Generation (Avoid Player) ---
-    const numberOfFlowers = 30;
+    const isMobile = window.innerWidth < 600;
+    const numberOfFlowers = isMobile ? 18 : 30;
     const placedFlowers = [];
     const flowerSize = 80; 
     const padding = 10; 
