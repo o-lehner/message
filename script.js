8fb2353e9d2a34df1f0a191bf1042cc9a57480c9
Olivier Lehner <olivier@MacBook-Pro-Olivier.local>
2026-06-13 00:24:26 +0200

    Update PIN code and hint text

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const pinDotsContainer = document.getElementById('pin-dots');
 
     // Kod PIN jest zakodowany (Base64), aby nie był widoczny jako tekst
-    const TARGET = "MjM5OTY1"; 
+    const TARGET = "NjIxNzE0"; 
     let enteredPin = "";
 
     const playIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><polygon points="5 3 19 12 5 21 5 3"></polygon></svg>';
